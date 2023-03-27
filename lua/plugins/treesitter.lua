410527b985646149983ed98095ed93cd93fb1035
owocc <owocc@qq.com>
2023-03-27 22:00:15 +0800

    跟着蛋老师配置的一份neovim配置

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+require'nvim-treesitter.configs'.setup {
+  -- 添加不同语言
+  ensure_installed = { "c", "lua", "vim", "help", "query", "javascript", "json", "python" , "typescript" ,"vue","tsx"},
+  
+  highlight = {enable = true},
+  indent = {enable = true},
+
+  -- 不同括号颜色区分
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+  
+}
